0eb9d34f7ff25c168f1830f12e3f60cae3444ae9
jistank <giorgith01@gmail.com>
2026-06-03 20:27:57 +0200

    Update security config

--- backend/src/main/java/com/ota/app/config/SecurityConfig.java
+++ backend/src/main/java/com/ota/app/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ota.app.config;
 
+import com.ota.app.service.CustomOAuth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
@@ -11,53 +12,43 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	
 	@Autowired
-	
 	private JwtFilter jwtFilter;
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() { // using Argon2 for password hashing
-	    return new Argon2PasswordEncoder(
-	        16,    // length of salt
-	        32,    // length of hash
-	        1,     // parallelism (number of threads)
-	        65536, // memory (64MB)
-	        3      // iterations
-	    );
-	}
+	@Autowired
+	private CustomOAuth2UserService customOAuth2UserService;
+	
+	@Autowired
+	private OAuth2LoginSuccessHandler oauth2LoginSuccessHandler;
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http.csrf(customizer -> customizer.disable())
 				.authorizeHttpRequests(request -> request
-			    .requestMatchers(HttpMethod.POST, "/users","/users/login").permitAll() // allow anyone to create a new user
-			    .anyRequest().authenticated()) // all other endpoints require authentication
-				//.authorizeHttpRequests(request -> request.anyRequest().authenticated()) // all endpoints require authentication
+			    .requestMatchers(HttpMethod.POST, "/users","/users/login").permitAll()
+			    .anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+				.oauth2Login(oauth2 -> oauth2
+				    .userInfoEndpoint(userInfo -> userInfo
+				        .userService(customOAuth2UserService))
+				    .successHandler(oauth2LoginSuccessHandler));
 
 		return http.build();
 	}
-	
 	
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
 		return config.getAuthenticationManager();
 	}
-	
-	
-	
-	
 	
 }
